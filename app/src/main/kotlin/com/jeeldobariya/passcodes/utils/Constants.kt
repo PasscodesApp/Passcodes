d566d6098bbd788ef6bea684938db09482f55f22
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-24 21:38:30 +0530

    fix: soon to be broken URLgit add .

--- app/src/main/kotlin/com/jeeldobariya/passcodes/utils/Constants.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/utils/Constants.kt
@@ -2,14 +2,18 @@ package com.jeeldobariya.passcodes.utils
 
 object Constant {
     const val REPOSITORY_SIGNATURE = "PasscodesApp/Passcodes"
+    const val DOCS_REPOSITORY_SIGNATURE = "PasscodesApp/Passcodes-Docs"
 
-    // URL Constants
+    // Main URL Constants
     const val TELEGRAM_COMMUNITY_URL = "https://t.me/passcodescommunity"
     const val REPOSITORY_URL = "https://github.com/$REPOSITORY_SIGNATURE"
+    const val DOCS_REPOSITORY_URL = "https://github.com/$DOCS_REPOSITORY_SIGNATURE"
+
+    // URL Constants
     const val GITHUB_RELEASE_API_URL = "https://api.github.com/repos/$REPOSITORY_SIGNATURE/releases"
     const val REPORT_BUG_URL = "$REPOSITORY_URL/issues/new?template=bug-report.md"
-    const val RELEASE_NOTE_URL = "$REPOSITORY_URL/blob/main/docs/release-notes.md"
-    const val SECURITY_GUIDE_URL = "$REPOSITORY_URL/blob/main/docs/security-guide.md"
+    const val RELEASE_NOTE_URL = "$DOCS_REPOSITORY_URL/blob/main/user-docs/release-notes.md"
+    const val SECURITY_GUIDE_URL = "$DOCS_REPOSITORY_URL/blob/main/user-docs/security-guidelines.md"
 
 
     // Shared Preferences Constants
@@ -19,3 +23,4 @@ object Constant {
     const val FEATURE_FLAGS_PREFS_NAME = "feature_flags_prefs"
     const val LATEST_FEATURES_KEY = "latest_features_enabled"
 }
+
