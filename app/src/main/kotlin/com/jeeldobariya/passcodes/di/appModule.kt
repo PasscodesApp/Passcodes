6c30299809ee6218e54e224334441507129f6019
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-10-07 18:43:27 +0530

    feat: implement dependency injection with koin

---
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/di/appModule.kt
@@ -0,0 +1,19 @@
+package com.jeeldobariya.passcodes.di
+
+import com.jeeldobariya.passcodes.ui.UpdatePasswordViewModel
+import com.jeeldobariya.passcodes.utils.Controller
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module{
+
+    single {
+        Controller(androidContext())
+    }
+
+    viewModel {
+        UpdatePasswordViewModel(get())
+    }
+
+}
