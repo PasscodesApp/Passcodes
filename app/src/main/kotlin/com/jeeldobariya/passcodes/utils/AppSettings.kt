50a32e3e30ec10316599e11e00847dba4ba071b5
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-11-21 11:37:59 +0530

    chore: unverified solution for data store migration....

--- app/src/main/kotlin/com/jeeldobariya/passcodes/utils/AppSettings.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/utils/AppSettings.kt
@@ -1,5 +1,7 @@
 package com.jeeldobariya.passcodes.utils
 
+// please refer to `app/src/main/kotlin/com/jeeldobariya/passcodes/flags/FeatureFlagsSettings.kt` for Migration Guide.
+
 import android.content.Context
 import androidx.datastore.dataStore
 import com.jeeldobariya.passcodes.R
