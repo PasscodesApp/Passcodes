7503b5af75a41f60a8ee897a779c05e3600a9c2b
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-11-29 22:07:00 +0530

    refactor: use string to uri extention function

--- autofill/src/main/kotlin/com/jeeldobariya/passcodes/autofill/AutofillSettingsActivity.kt
+++ autofill/src/main/kotlin/com/jeeldobariya/passcodes/autofill/AutofillSettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.google.android.material.button.MaterialButton
 
 class AutofillSettingsActivity : AppCompatActivity() {
@@ -15,7 +16,7 @@ class AutofillSettingsActivity : AppCompatActivity() {
         val enableAutofillButton = findViewById<MaterialButton>(R.id.enable_autofill_button)
         enableAutofillButton.setOnClickListener {
             val intent = Intent(Settings.ACTION_REQUEST_SET_AUTOFILL_SERVICE)
-            intent.data = android.net.Uri.parse("package:$packageName")
+            intent.data = "package:$packageName".toUri()
             startActivity(intent)
         }
     }
