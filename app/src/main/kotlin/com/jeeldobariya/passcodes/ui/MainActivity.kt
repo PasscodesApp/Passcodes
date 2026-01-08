4671b62af21d689b4910130325ab79d3220b2197
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-01-08 20:37:08 +0530

    feat: finally made the first screen in jetpack compose

--- app/src/main/kotlin/com/jeeldobariya/passcodes/ui/MainActivity.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/ui/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jeeldobariya.passcodes.R
 import com.jeeldobariya.passcodes.ui.ui.theme.PasscodesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PasscodesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(stringResource(R.string.app_name), fontSize = 32.sp)
+
+            Text(stringResource(R.string.app_version), fontSize = 12.sp)
+
+            Spacer(Modifier.padding(48.dp))
+
+            Button(onClick = { /* TODO */ }) {
+                Text(stringResource(R.string.password_manager_button_text))
+            }
+
+            FilledTonalButton(onClick = { /* TODO */ }) {
+                Text(stringResource(R.string.setting_button_text))
+            }
+
+            FilledTonalButton(onClick = { /* TODO */ }) {
+                Text(stringResource(R.string.about_us_button_text))
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
+@PreviewLightDark
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     PasscodesTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
