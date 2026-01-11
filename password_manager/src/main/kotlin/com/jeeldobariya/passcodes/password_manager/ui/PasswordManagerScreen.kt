075a29dfca034eae2bc051fdf939037911a9c175
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-01-11 20:48:42 +0530

    feat: made password manager new ui functional

--- password_manager/src/main/kotlin/com/jeeldobariya/passcodes/password_manager/ui/PasswordManagerScreen.kt
+++ password_manager/src/main/kotlin/com/jeeldobariya/passcodes/password_manager/ui/PasswordManagerScreen.kt
@@ -14,39 +14,67 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import com.jeeldobariya.passcodes.password_manager.presentation.load_password.LoadPasswordAction
+import com.jeeldobariya.passcodes.password_manager.presentation.load_password.LoadPasswordViewModel
+import kotlinx.coroutines.launch
+import org.koin.compose.viewmodel.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PasswordManagerScreen() {
+fun PasswordManagerScreen(viewmodel: LoadPasswordViewModel = koinViewModel()) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             TopAppBar(
                 title = { Text("Password Manager") },
                 navigationIcon = {
-                    IconButton(onClick = { }) {
+                    IconButton(onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("Coming Soon")
+                        }
+                    }) {
                         Icon(imageVector = Icons.AutoMirrored.Default.ArrowBack, contentDescription = "")
                     }
                 }
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = { }) {
+            FloatingActionButton(onClick = {
+                scope.launch {
+                    snackbarHostState.showSnackbar("Coming Soon")
+                }
+            }) {
                 Icon(imageVector = Icons.Default.AddCircle, contentDescription = "")
             }
         }
     ) { paddingValue ->
+        viewmodel.onAction(LoadPasswordAction.RefreshPassword)
+        val state = viewmodel.state.collectAsState()
+
         LazyColumn(modifier = Modifier.padding(paddingValue)) {
-            (1..10).forEach {
+            (state.value.passwordEntityList).forEach {
                 item {
-                    Column(modifier = Modifier.padding(12.dp).fillMaxSize()) {
-                        Text("Domain $it", style = MaterialTheme.typography.titleLarge)
-                        Text("Username $it", style = MaterialTheme.typography.bodySmall)
+                    Column(
+                        modifier = Modifier
+                            .padding(12.dp)
+                            .fillMaxSize()
+                    ) {
+                        Text(text = it.domain, style = MaterialTheme.typography.titleLarge)
+                        Text(text = it.username, style = MaterialTheme.typography.bodySmall)
                     }
 
                     HorizontalDivider(thickness = 2.dp)
