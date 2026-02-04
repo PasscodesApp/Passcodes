ce3aba8934f0105c570192ed9a7c14725d70f791
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-02-04 19:23:34 +0530

    fix: error

--- app/src/main/kotlin/com/jeeldobariya/passcodes/ui/MainActivity.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/ui/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PasscodesTheme {
-                NavigationRoot()
+                NavigationRoot(modernLayout = true)
             }
         }
     }
