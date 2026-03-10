b946deed0bfe13fcc9520ab1e373785afca98076
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-03-10 18:42:33 +0530

    refactor: modularize navigation code using entry provider scope dsl of navigation 3

--- app/src/main/kotlin/com/jeeldobariya/passcodes/navigation/NavigationRoot.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/navigation/NavigationRoot.kt
@@ -14,20 +14,8 @@ import androidx.navigation3.ui.NavDisplay
 import com.jeeldobariya.passcodes.core.datastore.AppSettings
 import com.jeeldobariya.passcodes.core.datastore.appDatastore
 import com.jeeldobariya.passcodes.core.navigation.Route
-import com.jeeldobariya.passcodes.password_manager.presentation.load_password.ClassicalLoadPasswordScreen
-import com.jeeldobariya.passcodes.password_manager.presentation.password_manager.ClassicalPasswordManagerScreen
-import com.jeeldobariya.passcodes.password_manager.presentation.password_manager.ModernPasswordManagerScreen
-import com.jeeldobariya.passcodes.password_manager.presentation.save_password.ClassicalSavePasswordScreen
-import com.jeeldobariya.passcodes.password_manager.presentation.save_password.ModernSavePasswordScreen
-import com.jeeldobariya.passcodes.password_manager.presentation.update_password.ClassicalUpdatePasswordScreen
-import com.jeeldobariya.passcodes.password_manager.presentation.update_password.ModernUpdatePasswordScreen
-import com.jeeldobariya.passcodes.password_manager.presentation.view_password.ClassicalViewPasswordScreen
-import com.jeeldobariya.passcodes.presentation.about_screen.ClassicalAboutScreen
-import com.jeeldobariya.passcodes.presentation.main_screen.ClassicalMainScreen
-import com.jeeldobariya.passcodes.presentation.setting_screen.ClassicalSettingsScreen
-import com.jeeldobariya.passcodes.presentation.about_screen.ModernAboutScreen
-import com.jeeldobariya.passcodes.presentation.main_screen.ModernMainScreen
-import com.jeeldobariya.passcodes.presentation.setting_screen.ModernSettingsScreen
+import com.jeeldobariya.passcodes.password_manager.navigation.classicalPasswordManagerEntriesProvider
+import com.jeeldobariya.passcodes.password_manager.navigation.modernPasswordManagerEntriesProvider
 
 
 @Composable
@@ -42,37 +30,8 @@ private fun ModernNavigationRoot(backStack: NavBackStack<NavKey>, navigateTo: (R
             rememberViewModelStoreNavEntryDecorator()
         ),
         entryProvider = entryProvider {
-            entry<Route.Home> {
-                ModernMainScreen(navigateTo)
-            }
-
-            entry<Route.Settings> {
-                ModernSettingsScreen()
-            }
-
-            entry<Route.AboutUs> {
-                ModernAboutScreen()
-            }
-
-            entry<Route.PasswordManager> {
-                ModernPasswordManagerScreen(navigateTo)
-            }
-
-            entry<Route.LoadPassword> {
-                ModernPasswordManagerScreen(navigateTo)
-            }
-
-            entry<Route.SavePassword> {
-                ModernSavePasswordScreen()
-            }
-
-            entry<Route.ViewPassword> { // Theoretically, Should be treated as `UpdatePassword` Route in Modern Layout...
-                ModernUpdatePasswordScreen(it.id)
-            }
-
-            entry<Route.UpdatePassword> {
-                ModernUpdatePasswordScreen(it.id)
-            }
+            modernMainAppEntriesProvider(navigateTo = navigateTo)
+            modernPasswordManagerEntriesProvider(navigateTo = navigateTo)
         }
     )
 }
@@ -89,37 +48,8 @@ private fun ClassicalNavigationRoot(backStack: NavBackStack<NavKey>, navigateTo:
             rememberViewModelStoreNavEntryDecorator()
         ),
         entryProvider = entryProvider {
-            entry<Route.Home> {
-                ClassicalMainScreen(navigateTo = navigateTo)
-            }
-
-            entry<Route.Settings> {
-                ClassicalSettingsScreen()
-            }
-
-            entry<Route.AboutUs> {
-                ClassicalAboutScreen()
-            }
-
-            entry<Route.PasswordManager> {
-                ClassicalPasswordManagerScreen(navigateTo = navigateTo)
-            }
-
-            entry<Route.LoadPassword> {
-                ClassicalLoadPasswordScreen(navigateTo = navigateTo)
-            }
-
-            entry<Route.SavePassword> {
-                ClassicalSavePasswordScreen()
-            }
-
-            entry<Route.ViewPassword> {
-                ClassicalViewPasswordScreen(passwordId = it.id, navigateTo = navigateTo)
-            }
-
-            entry<Route.UpdatePassword> {
-                ClassicalUpdatePasswordScreen(passwordId = it.id)
-            }
+            classicalMainAppEntriesProvider(navigateTo = navigateTo)
+            classicalPasswordManagerEntriesProvider(navigateTo = navigateTo)
         }
     )
 }
