77892c8060e87848696fba731522990b44efed12
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-03-10 16:56:56 +0530

    feat: made a default material3 theme

--- design_system/src/commonMain/kotlin/com/jeeldobariya/passcodes/design_system/theme/Color.kt
+++ design_system/src/commonMain/kotlin/com/jeeldobariya/passcodes/design_system/theme/Color.kt
@@ -2,10 +2,26 @@ package com.jeeldobariya.passcodes.design_system.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Primary
+val Purple40 = Color(0xFF6750A4)
 val Purple80 = Color(0xFFD0BCFF)
+
+// Secondary
+val PurpleGrey40 = Color(0xFF625B71)
 val PurpleGrey80 = Color(0xFFCCC2DC)
+
+// Tertiary
+val Pink40 = Color(0xFF7D5260)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Background
+val BackgroundLight = Color(0xFFFFFBFE)
+val BackgroundDark = Color(0xFF1C1B1F)
+
+// Surface
+val SurfaceLight = Color(0xFFFFFBFE)
+val SurfaceDark = Color(0xFF1C1B1F)
+
+// Error
+val ErrorLight = Color(0xFFB3261E)
+val ErrorDark = Color(0xFFF2B8B5)
