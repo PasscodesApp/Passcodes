9907a9daa13e8ff793fa69a31dc43a65bbfa66b3
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-04-14 12:01:11 +0530

    feat: make new main activity check for updates

--- app/src/main/kotlin/com/jeeldobariya/passcodes/MainActivity.kt
+++ app/src/main/kotlin/com/jeeldobariya/passcodes/MainActivity.kt
@@ -8,16 +8,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.lifecycleScope
+import com.jeeldobariya.passcodes.core.domain.usecases.CheckForUpdateUseCase
 import com.jeeldobariya.passcodes.core.feature_flags.FeatureFlagsSettings
 import com.jeeldobariya.passcodes.core.feature_flags.featureFlagsDatastore
 import com.jeeldobariya.passcodes.design_system.theme.PasscodesTheme
 import com.jeeldobariya.passcodes.navigation.NavigationRoot
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+
+    private val checkForUpdateUseCase: CheckForUpdateUseCase by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
+        lifecycleScope.launch(Dispatchers.IO) {
+            checkForUpdateUseCase(
+                currentVersion = BuildConfig.VERSION_NAME,
+                githubReleaseApiUrl = Constant.GITHUB_RELEASE_API_URL,
+                telegramCommunityUrl = Constant.TELEGRAM_COMMUNITY_URL
+            )
+        }
+
+        enableEdgeToEdge()
         setContent {
             PasscodesTheme {
                 val featureFlagState by featureFlagsDatastore.data.collectAsState(
