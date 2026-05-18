9b60b2955e6793a18c3a67bea78f49eb325edbe3
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-05-18 23:08:56 +0530

    feat: detect the android room database existance in expo-react-native
    Signed-off-by: Jeel Dobariya <jeeldobariya38@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,39 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import { File } from "expo-file-system";
 import { Stack } from "expo-router";
 import { SQLiteProvider, openDatabaseSync } from "expo-sqlite";
 import { Suspense } from "react";
 import { ActivityIndicator, Text, View } from "react-native";
 import migrations from "../db/drizzle/migrations";
 
+async function migrateOldAndroidData() {
+  console.log("----- Migrating Android Old Data To Expo -----");
+
+  const packageName = "com.jeeldobariya.passcodes.dev"; // Replace with your Android application ID
+  const dbName = "master.db"; // Replace with your Room database name
+
+  // 1. Construct the exact URI
+  const dbDirUri = `file:///data/data/${packageName}/databases/`;
+  console.log(dbDirUri);
+
+  // 2. Check if the file exists before performing operations
+  const roomDbFile = new File(dbDirUri, dbName);
+  if (!roomDbFile.exists) {
+    console.warn("room database file not found at:", roomDbFile);
+    // return; // TODO: just for debugging. must throw a error instead.
+    // throw new Error("Database not found at: " + dbUri);
+  }
+
+  console.log("room database file found at:", roomDbFile.uri);
+}
+
 const DATABASE_NAME = "test2.db";
 
 export default function RootLayout() {
   const expoDb = openDatabaseSync(DATABASE_NAME);
+  migrateOldAndroidData();
+
   const db = drizzle(expoDb);
 
   const { success, error } = useMigrations(db, migrations);
