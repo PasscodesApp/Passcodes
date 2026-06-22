94d2dd278aa2e0fd3a2a7dbed374c80dc15bb381
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-06-22 10:42:33 +0530

    feat: make a feature to share to google passwords
    Signed-off-by: Jeel Dobariya <jeeldobariya38@gmail.com>

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,16 +1,33 @@
 import LinkButton from "@/components/LinkButton";
 import ScreenHeading from "@/components/ScreenHeading";
+import { passwords } from "@/db/schema";
 import {
   isBiometricsAuthEnabled,
   toggleBiometricsFeature,
 } from "@/libs/biometric";
+import {
+  getGooglePasswordsCSVContent,
+  sharePasswordAsCSV,
+} from "@/libs/exporting";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { useSQLiteContext } from "expo-sqlite";
 import { useState } from "react";
-import { ScrollView, Switch, Text, View } from "react-native";
+import { Button, ScrollView, Switch, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SettingsScreen() {
   const [isEnabled, setIsEnabled] = useState(isBiometricsAuthEnabled());
   const toggleSwitch = () => setIsEnabled(toggleBiometricsFeature());
+
+  let db = useSQLiteContext();
+
+  async function handleExportPasswords() {
+    const drizzleDb = drizzle(db);
+    const result = await drizzleDb.select().from(passwords);
+
+    let content = await getGooglePasswordsCSVContent(result);
+    sharePasswordAsCSV(content);
+  }
 
   return (
     <SafeAreaView style={{ padding: 20 }}>
@@ -37,6 +54,21 @@ export default function SettingsScreen() {
             trackColor={{ false: "#767577", true: "#81b0ff" }}
             thumbColor={isEnabled ? "#25f068" : "#4d2b05"}
             ios_backgroundColor="#3e3e3e"
+          />
+        </View>
+
+        <View
+          style={{
+            backgroundColor: "#efe9e3",
+            borderWidth: 2,
+            padding: 16,
+            borderRadius: 20,
+            alignItems: "center",
+          }}
+        >
+          <Button
+            title="Share to Google Passwords"
+            onPress={() => handleExportPasswords()}
           />
         </View>
 
@@ -75,14 +107,18 @@ export default function SettingsScreen() {
         >
           <Text style={{ fontSize: 32 }}>Contact Us</Text>
           <LinkButton
+            style={{
+              color: "#004292",
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+            href={"https://discord.gg/kSSkYq7KAQ"}
+            text="Join Discord"
+          />
+          <LinkButton
             style={{ color: "#0257c1", textAlign: "center" }}
             href={"https://passcodesapp.github.io/Passcodes-Website/"}
             text="Goto Website"
-          />
-          <LinkButton
-            style={{ color: "#004292", textAlign: "center" }}
-            href={"https://discord.gg/kSSkYq7KAQ"}
-            text="Join Discord"
           />
           <LinkButton
             style={{ color: "#0257c1", textAlign: "center" }}
---
+++ src/libs/exporting.ts
@@ -0,0 +1,38 @@
+import { File, Paths } from "expo-file-system";
+import * as Sharing from "expo-sharing";
+
+type Password = {
+  domain: string;
+  url: string | null;
+  username: string;
+  password: string;
+  notes: string | null;
+};
+
+export async function getGooglePasswordsCSVContent(passwords: Password[]) {
+  let result = ["name,url,username,password,note"];
+
+  passwords.forEach((password) => {
+    let parseURL = password.url || `https://local.${password.domain}`;
+
+    let CSVRow = `${password.domain},${parseURL},${password.username},${password.password},${password.notes || ""}`;
+    result.push(CSVRow);
+  });
+
+  return result.join("\n");
+}
+
+export async function sharePasswordAsCSV(csvContent: string) {
+  const file = new File(Paths.cache, `passcodes-export-${Date.now()}.csv`);
+
+  file.write(csvContent);
+
+  try {
+    await Sharing.shareAsync(file.uri, {
+      mimeType: "text/csv",
+      dialogTitle: "Export Passwords",
+    });
+  } finally {
+    file.delete();
+  }
+}
