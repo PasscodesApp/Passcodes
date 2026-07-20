7ee95fe69bb02adbc3ecbe92c2291d9ac25fc4b2
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-07-20 21:01:03 +0530

    feat: add bottomsheet for new passwords
    Signed-off-by: Jeel Dobariya <jeeldobariya38@gmail.com>

--- src/app/(password-manager)/_layout.tsx
+++ src/app/(password-manager)/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
 
 export default function PasswordManagerLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: "Password Manager" }} />
+
+      <Stack.Screen
+        name="password-details"
+        options={{ title: "Password Details" }}
+      />
+
+      <Stack.Screen
+        name="save-password"
+        options={{
+          title: "New Password",
+          presentation: "formSheet",
+          sheetAllowedDetents: [0.35, 0.95],
+          sheetGrabberVisible: true,
+          headerShown: false,
+          sheetCornerRadius: 20,
+        }}
+      />
+    </Stack>
+  );
 }
