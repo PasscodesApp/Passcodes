e930e5a656f53b5e82f77ff2c9ef246fa3bf0a3f
Jeel Dobariya <jeeldobariya38@gmail.com>
2026-08-29 22:34:01 +0530

    chore: update dependency to latest.
    Signed-off-by: Jeel Dobariya <jeeldobariya38@gmail.com>

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,8 +1,8 @@
 export default {
   appname: "Passcodes",
-  version: "v4.0.0 - RC9",
+  version: "v4.0.0 - RC.1",
   build: {
-    version: "v4.0.0-RC9",
+    version: "v4.0.0-RC.1",
     versionCodeAndroid: 10,
     versionCodeIos: "1.0.0",
   },
